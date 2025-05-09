fe76fcc161e33e542c5d618abe86c2cf51ec212e
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-05-09 09:52:46 +0200

    MET-6467 Sonar issues

--- metis-core-common/src/main/java/eu/europeana/metis/core/engine/base/task/input/InputDataEndpoint.java
+++ metis-core-common/src/main/java/eu/europeana/metis/core/engine/base/task/input/InputDataEndpoint.java
@@ -3,6 +3,8 @@ package eu.europeana.metis.core.engine.base.task.input;
 /**
  * Represents an input data endpoint used in the processing engine.
  */
+//False positive: Check https://community.sonarsource.com/t/s7027-prevents-from-using-sealed-classes/133423/5
+@SuppressWarnings("javaarchitecture:S7027")
 public sealed interface InputDataEndpoint
     permits HttpHarvestInputDataEndpoint, OaiHarvestInputDataEndpoint, InternalInputDataEndpoint {
 
