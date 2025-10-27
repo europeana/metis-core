c338c2bf085a7203a8f6dd6e7ea4ba4811df4f55
Jorge Ortiz <jorge.ortiz@europeana.eu>
2025-10-27 16:11:31 +0100

    MET-6789 update realm considering local and postman, also update system ids user names

--- metis-core-service/src/main/java/eu/europeana/metis/core/service/UserService.java
+++ metis-core-service/src/main/java/eu/europeana/metis/core/service/UserService.java
@@ -2,6 +2,7 @@ package eu.europeana.metis.core.service;
 
 import eu.europeana.metis.core.user.User;
 import eu.europeana.metis.core.user.User.UserBuilder;
+import eu.europeana.metis.core.workflow.execution.SystemId;
 import jakarta.ws.rs.NotFoundException;
 import java.lang.invoke.MethodHandles;
 import java.time.Instant;
@@ -23,6 +24,8 @@ public class UserService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private static final String UNKNOWN_USER_NAME = "Unknown user";
+  private static final String SYSTEM_MINUTE_CAP_EXPIRE_NAME = "Metis system initiated expiration";
+  private static final String STARTED_BY_SYSTEM_NAME = "Metis system initiated";
   private static final ConcurrentHashMap<String, User> USER_CACHE = new ConcurrentHashMap<>();
   private final Keycloak keycloak;
   private final String realm;
@@ -91,8 +94,16 @@ public class UserService {
     UserBuilder userBuilder = new UserBuilder();
 
     if (userRepresentation.isEmpty()) {
+      String userName;
+      if (userId.equals(SystemId.STARTED_BY_SYSTEM.name())) {
+        userName = STARTED_BY_SYSTEM_NAME;
+      } else if (userId.equals(SystemId.SYSTEM_MINUTE_CAP_EXPIRE.name())) {
+        userName = SYSTEM_MINUTE_CAP_EXPIRE_NAME;
+      } else {
+        userName = UNKNOWN_USER_NAME;
+      }
       userBuilder.userId(userId)
-                 .userName(UNKNOWN_USER_NAME)
+                 .userName(userName)
                  .issuedAt(Instant.now());
     } else {
       userBuilder.userId(userRepresentation.get().getId())
